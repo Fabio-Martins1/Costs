602788f597916d21872a63465fe8aeb2ed7699bd
Fabio Martins <kiritosannee@gmail.com>
2022-08-24 05:11:16 -0300

    firs comitt

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,15 @@
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+
+export default function NewProject(){
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os seus serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+        
+    )
+}
